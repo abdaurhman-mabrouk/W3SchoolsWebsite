8b661dcc31c76b183783c65383075ced0785f5e0
abdaurhman-mabrouk <aabdo6629@gmail.com>
2024-07-10 01:38:06 +0300

    add First Top Navbar Links Divs and Some of its JS Logic

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 //Constants
 const sTopNav = document.getElementById("sTopNav")
+const fTopNav = document.getElementById("fTopNav")
 const leftArrow = document.getElementById('SL');
 const rightArrow = document.getElementById('SR');
+
+const tutorialsLink = document.getElementById("tutorialsLink")
+const hiddenContentDiv = document.getElementById("hiddenContentDiv")
+const tutorialsDiv = document.getElementById("TutorialsDiv")
+
+tutorialsLink.onclick = () => {
+    console.log("Working")
+
+    tutorialsDiv.style.display = `block`
+    tutorialsDiv.focus()
+    sTopNav.style.zIndex = "-1"
+}
