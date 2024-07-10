d91bbc18e9ecdb23cb2a342329e270f7845ff527
abdaurhman-mabrouk <aabdo6629@gmail.com>
2024-07-10 06:42:11 +0300

    Code Optimizations (HTML and CSS)

--- main.js
+++ main.js
@@ -6,12 +6,20 @@ const rightArrow = document.getElementById('SR');
 
 const tutorialsLink = document.getElementById("tutorialsLink")
 const hiddenContentDiv = document.getElementById("hiddenContentDiv")
-const tutorialsDiv = document.getElementById("TutorialsDiv")
+const tutorialsDiv = document.getElementById("tutorialsDiv")
+const tutorialsHeaderDiv = document.querySelector("tutorialsDiv div.headerDiv")
+const tutorialsSearchField = document.querySelector("tutorialsDiv div.headerDiv input[type='search']")
 
 tutorialsLink.onclick = () => {
-    console.log("Working")
+    let divDisplay = tutorialsDiv.style.display
+    if (divDisplay == `` || divDisplay == `block`) {
+        tutorialsDiv.style.display = `none`
+        sTopNav.style.zIndex = "2"
+        tutorialsLink.blur()
 
-    tutorialsDiv.style.display = `block`
-    tutorialsDiv.focus()
-    sTopNav.style.zIndex = "-1"
+    } else {
+        tutorialsDiv.style.display = ``
+        sTopNav.style.zIndex = "-1"
+        tutorialsDiv.focus()
+    }
 }
