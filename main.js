defb914c5fb374dd23aeb0662a9bbbfab01dadc1
abdaurhman-mabrouk <aabdo6629@gmail.com>
2024-07-11 18:19:13 +0300

    Editions

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ const leftArrow = document.getElementById('SL');
 const rightArrow = document.getElementById('SR');
 
 const tutorialsLink = document.getElementById("tutorialsLink")
+
 const hiddenContentDiv = document.getElementById("hiddenContentDiv")
 const tutorialsDiv = document.getElementById("tutorialsDiv")
 const tutorialsHeaderDiv = document.querySelector("tutorialsDiv div.headerDiv")
+
 const tutorialsSearchField = document.querySelector("tutorialsDiv div.headerDiv input[type='search']")
 
 tutorialsLink.onclick = () => {
@@ -15,12 +17,10 @@ tutorialsLink.onclick = () => {
 
     if (divDisplay == `` || divDisplay == `block`) {
         tutorialsDiv.style.display = `none`
-        sTopNav.style.zIndex = "2"
         tutorialsLink.blur()
 
     } else {
-        tutorialsDiv.style.display = ``
-        sTopNav.style.zIndex = "-1"
+        tutorialsDiv.style.display = `block`
         tutorialsDiv.focus()
     }
 }
